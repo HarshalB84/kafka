6d00f07db2464c575105c5e3b32aeb115cd6df1f
HarshalB84 <harshalbhoyar84@gmail.com>
2026-03-31 23:33:53 +0530

    Send JSON messages using Kafka

--- notification-service/src/main/java/com/example/learnKafka/notification_service/consumer/UserKafkaConsumer.java
+++ notification-service/src/main/java/com/example/learnKafka/notification_service/consumer/UserKafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.example.learnKafka.notification_service.consumer;
 
+import com.example.learnKafka.notification_service.event.UserCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,24 +9,29 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserKafkaConsumer {
 
+    @KafkaListener(topics = "user-created-topic")
+    public void handleUserCreated(UserCreatedEvent userCreatedEvent) {
+        log.info("handleUserCreated: {}", userCreatedEvent);
+    }
+
     @KafkaListener(topics = "user-random-topic")
     public void handleUserRandomMessage1(String message){
 
-        log.info("Received user random message: {}", message);
+        log.info("handleUserRandomMessage1: {}", message);
 
     }
 
     @KafkaListener(topics = "user-random-topic")
     public void handleUserRandomMessage2(String message){
 
-        log.info("Received user random message: {}", message);
+        log.info("handleUserRandomMessage2: {}", message);
 
     }
 
     @KafkaListener(topics = "user-random-topic")
     public void handleUserRandomMessage3(String message){
 
-        log.info("Received user random message: {}", message);
+        log.info("handleUserRandomMessage3: {}", message);
 
     }
 
--- user-service/src/main/java/com/example/learnKafka/user_service/controller/UserController.java
+++ user-service/src/main/java/com/example/learnKafka/user_service/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.learnKafka.user_service.controller;
 
+import com.example.learnKafka.user_service.dto.CreateUserRequestDto;
+import com.example.learnKafka.user_service.event.UserCreatedEvent;
+import com.example.learnKafka.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,12 +18,20 @@ public class UserController {
 
     @Value("${kafka.topic.user-random-topic}")
     private String KAFKA_RANDOM_USER_TOPIC;
-
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody CreateUserRequestDto createUserRequestDto) {
+        userService.createUser(createUserRequestDto);
+        return ResponseEntity.ok("User is created");
+    }
 
     @PostMapping("/{message}")
     public ResponseEntity<String> sendMessage(@PathVariable String message){
-        kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC, message);
+        for (int i = 0; i < 1000; i++) {
+            kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC, message+i);
+        }
         return ResponseEntity.ok("Message queued");
     }
 
